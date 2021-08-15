314d2ce7e4c25fc6b8f27940bc231bced205db8e
giggity-hub <imigstefan@gmail.com>
2021-08-16 00:01:52 +0200

    did some stuff

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,10 +1,42 @@
 <script>
+import { onMount } from "svelte";
+import pickRandom from 'pick-random';
+
 	export let name;
+	
+	let dirTea;
+
+	let adjectives;
+	let foods;
+
+	function timeout(ms) {
+    	return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	function generateTeaName(){
+		let adj = pickRandom(adjectives);
+		let food = pickRandom(foods);
+
+		dirTea = adj + " " + food
+	}
+
+	onMount(async () => {
+		adjectives = await 	fetch('https://raw.githubusercontent.com/giggity-hub/ice-tea-name-generator/main/Jupyter%20Notebook/NEG_ADJ_EN.txt')
+			.then(res => res.text())
+			.then(text => text.split("\n"))
+
+		foods = await 	fetch('https://raw.githubusercontent.com/giggity-hub/ice-tea-name-generator/main/Jupyter%20Notebook/Food_Filtered.txt')
+			.then(res => res.text())
+			.then(text => text.split("\n"))
+			.then(words => words.map(word => word.replace("_", " ")))
+
+		console.log(adjectives);
+	})
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={generateTeaName} >Generate Dir-Tea</button>
+	<div>{dirTea}</div>
 </main>
 
 <style>
